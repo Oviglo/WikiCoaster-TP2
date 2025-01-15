3834ba5e1fa4b8aae72349c0de1741d1bb57a33e
Loic Ovigne <ovigne1@univ-lorraine.fr>
2025-01-15 11:17:28 +0100

    Voter

--- src/Security/Voter/CoasterVoter.php
+++ src/Security/Voter/CoasterVoter.php
@@ -24,22 +24,25 @@ final class CoasterVoter extends Voter
         $user = $token->getUser();
 
         // if the user is anonymous, do not grant access
-        if (!$user instanceof UserInterface) {
+        /*if (!$user instanceof UserInterface) {
             return false;
-        }
+        }*/
 
         // ... (check conditions and return true to grant permission) ...
-        switch ($attribute) {
+        /*switch ($attribute) {
             case self::EDIT:
-                    //return $subject->getAuthor() == $user || 
-                break;
+                return $subject->getAuthor() == $user;
 
             case self::VIEW:
                 // logic to determine if the user can VIEW
                 // return true or false
                 break;
-        }
+        }*/
 
-        return false;
+        return match ($attribute) {
+            self::EDIT => $subject->getAuthor() == $user,
+            self::VIEW => true,
+            default => false,
+        };
     }
 }
